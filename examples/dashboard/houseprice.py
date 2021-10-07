f3c08a75027606e55ea01403f962b4f6742a3836
ms4952 <mitali@saturncloud.io>
2021-10-07 09:26:48 -0700

    formatted apifile using black

--- examples/dashboard/houseprice.py
+++ examples/dashboard/houseprice.py
@@ -5,15 +5,20 @@ from sklearn.linear_model import LinearRegression
 app = FastAPI()
 
 
-@app.get('/predict')
+@app.get("/predict")
 async def predict(BedroomAbvGr: int = 3, YearBuilt: int = 2000):
-    df = pd.read_csv("https://saturn-public-data.s3.us-east-2.amazonaws.com/examples/dashboard/housePriceData.csv")
+    df = pd.read_csv(
+        "https://saturn-public-data.s3.us-east-2.amazonaws.com/examples/dashboard/housePriceData.csv"
+    )
     lr = LinearRegression()
-    lr.fit(df[['BedroomAbvGr', 'YearBuilt']], df['SalePrice'])
-    a = pd.DataFrame([[BedroomAbvGr, YearBuilt]], columns=['BedroomAbvGr', 'YearBuilt'])
+    lr.fit(df[["BedroomAbvGr", "YearBuilt"]], df["SalePrice"])
+    a = pd.DataFrame([[BedroomAbvGr, YearBuilt]], columns=["BedroomAbvGr", "YearBuilt"])
     v = lr.predict(a)
     if v <= 0:
-        raise HTTPException(status_code=400,
-                            detail="Please enter BedroomAbvGr between 0 and 8. Enter YearBuilt greater than 1872")
-    return {'prediction': v[0]}
-#this is the end of code
+
+        raise HTTPException(
+            status_code=400,
+            detail="Please enter BedroomAbvGr between 0 and 8. Enter YearBuilt greater than 1872",
+        )
+
+    return {"prediction": v[0]}
