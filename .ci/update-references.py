f2a9cd73c762577e323238e84543c23cb1a39b4e
Julia Signell <jsignell@gmail.com>
2021-11-08 16:30:24 -0500

    Remove reference before opening PR

--- .ci/update-references.py
+++ .ci/update-references.py
@@ -2,7 +2,7 @@ import json
 import os
 import sys
 
-REF = sys.argv[1]
+REF = sys.argv[1] if len(sys.argv) == 2 else None
 
 
 for file in os.listdir("examples"):
@@ -11,7 +11,10 @@ for file in os.listdir("examples"):
     with open(recipe_path) as f:
         recipe = json.load(f)
         for repo_dict in recipe["git_repositories"]:
-            repo_dict["reference"] = REF
+            if REF:
+                repo_dict["reference"] = REF
+            else:
+                repo_dict.pop("reference", None)
 
     with open(recipe_path, "w") as f:
         f.write(json.dumps(recipe, indent=2))
