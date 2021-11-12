59e1379173333581e6495cc47250b14f14c622f0
Julia Signell <jsignell@gmail.com>
2021-11-12 14:02:07 -0500

    Add a check to make sure that all working_dirs are valid (#199)

--- .ci/validate-examples.py
+++ .ci/validate-examples.py
@@ -32,7 +32,7 @@ FILENAME_REGEX = r"^[0-9A-Za-z\-\.]+$"
 SATURN_DIR_NAME = ".saturn"
 SATURN_JSON_NAME = "saturn.json"
 TEMPLATES_JSON_NAME = "templates.json"
-TOP_LEVEL_DIR = ARGS.examples_dir
+EXAMPLES_DIR = ARGS.examples_dir
 
 # This points to a json file in the saturncloud/recipe repo.
 RECIPE_SCHEMA_BRANCH = ARGS.recipe_schema_branch
@@ -76,6 +76,18 @@ def validate_recipe(schema, recipe_path):
     image_exists = image_exists_on_dockerhub(image_name=image_name, image_tag=image_tag)
     if not image_exists:
         raise ValidationError(f"image '{image_name}:{image_tag}' is not available on Docker Hub.")
+
+    working_dir = recipe["working_directory"]
+    working_dir_prefix = "/home/jovyan/git-repos/examples/examples/"
+    if not working_dir.startswith(working_dir_prefix):
+        raise ValidationError(
+            f"working_directory ('{working_dir}') needs to start with {working_dir_prefix}"
+        )
+
+    rel_path = working_dir.replace(working_dir_prefix, "")
+    abs_path = os.path.join(EXAMPLES_DIR, rel_path)
+    if not os.path.exists(abs_path):
+        raise ValidationError(f"working_directory ('{working_dir}') needs to point to a real path")
 
     if recipe.get("dask_cluster", None):
         if recipe["dask_cluster"]["num_workers"] > 3:
@@ -166,11 +178,11 @@ if __name__ == "__main__":
     res = requests.get(url=RECIPE_SCHEMA_URL)
     schema = res.json()
 
-    example_dirs = os.listdir(TOP_LEVEL_DIR)
+    example_dirs = os.listdir(EXAMPLES_DIR)
     if len(example_dirs) == 0:
-        ERRORS.add(f"No directories found under '{TOP_LEVEL_DIR}'")
+        ERRORS.add(f"No directories found under '{EXAMPLES_DIR}'")
 
-    templates_json = os.path.join(TOP_LEVEL_DIR, "..", ".saturn", TEMPLATES_JSON_NAME)
+    templates_json = os.path.join(EXAMPLES_DIR, "..", ".saturn", TEMPLATES_JSON_NAME)
     with open(templates_json, "r") as f:
         templates = json.load(f)["templates"]
 
@@ -202,11 +214,11 @@ if __name__ == "__main__":
 
     for example_dir in example_dirs:
         print(f"Working on directory '{example_dir}'")
-        full_dir = os.path.join(TOP_LEVEL_DIR, example_dir)
+        full_dir = os.path.join(EXAMPLES_DIR, example_dir)
 
         if not bool(re.search(DIRECTORY_REGEX, os.path.basename(example_dir))):
             msg = (
-                f"All directories under '{TOP_LEVEL_DIR}' should be named with only "
+                f"All directories under '{EXAMPLES_DIR}' should be named with only "
                 "lower alphanumeric characters and dashes. "
                 f"'{full_dir}` violates this rule."
             )
@@ -225,7 +237,7 @@ if __name__ == "__main__":
                 readme_file = os.path.join(fname, "README.md")
                 if not os.path.isfile(readme_file):
                     msg = (
-                        f"Every directory two-levels below '{TOP_LEVEL_DIR}' "
+                        f"Every directory two-levels below '{EXAMPLES_DIR}' "
                         f"must have a README.md. None found for '{fname}'."
                     )
                     ERRORS.add(msg)
